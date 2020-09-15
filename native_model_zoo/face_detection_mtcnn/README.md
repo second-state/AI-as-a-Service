5c43044cd214564120acac26b2e22645ea05d92f
Michael Yuan <michael@michaelyuan.com>
2020-09-15 01:39:25 +0000

    add the zoo

---
+++ native_model_zoo/face_detection_mtcnn/README.md
@@ -0,0 +1,35 @@
+# Face detection model wrapper
+
+This application is designed to be used as a `command` from a WebAssembly function running inside the SSVM to perform native tensorflow operations.
+It is NOT designed for direct command line use.
+
+## Install tensorflow
+
+[Follow the instructions](https://www.tensorflow.org/install/lang_c). On most Linux systems, just do the following.
+
+```bash
+$ wget https://storage.googleapis.com/tensorflow/libtensorflow/libtensorflow-gpu-linux-x86_64-1.15.0.tar.gz
+$ sudo tar -C /usr/ -xzf libtensorflow-gpu-linux-x86_64-1.15.0.tar.gz
+```
+
+## Build and install
+
+To install from the public repo, do this.
+
+```bash
+$ sudo cargo install face_detection_mtcnn
+```
+
+To install from the local source, do this.
+
+```bash
+$ cargo build --release
+$ sudo cp target/release/face_detection_mtcnn /usr/bin/
+```
+
+Note: If error occurs when building, try to update `rustup`.
+
+```bash
+$ rustup update nightly
+$ rustup update stable
+```
---
+++ native_model_zoo/face_detection_mtcnn/src/main.rs
@@ -0,0 +1,68 @@
+use serde_json::json;
+use std::env;
+use std::error::Error;
+use std::io::{self, Read};
+use tensorflow::{Graph, ImportGraphDefOptions, Session, SessionOptions, SessionRunArgs, Tensor};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let args: Vec<String> = env::args().collect();
+    let img_width: u64 = args[1].parse::<u64>().unwrap();
+    let img_height: u64 = args[2].parse::<u64>().unwrap();
+    let detection_threshold: f32 = args[3].parse::<f32>().unwrap();
+    let mut buffer: Vec<u8> = Vec::new();
+    let mut flattened: Vec<f32> = Vec::new();
+
+    // Parse stdin: vactor<flattended u8>.
+    io::stdin().read_to_end(&mut buffer)?;
+    for num in buffer {
+        flattened.push(num.into());
+    }
+
+    // Load up the graph as a byte array and create a tensorflow graph.
+    let model = include_bytes!("mtcnn.pb");
+    let mut graph = Graph::new();
+    graph.import_graph_def(&*model, &ImportGraphDefOptions::new())?;
+
+    // The `input` tensor expects BGR pixel data.
+    let input = Tensor::new(&[img_height, img_width, 3]).with_values(&flattened)?;
+    let min_size = Tensor::new(&[]).with_values(&[detection_threshold])?;
+
+    // Use input params from the existing module.
+    let thresholds = Tensor::new(&[3]).with_values(&[0.6f32, 0.7f32, 0.7f32])?;
+    let factor = Tensor::new(&[]).with_values(&[0.709f32])?;
+    let mut args = SessionRunArgs::new();
+
+    // Load default parameters and input image.
+    args.add_feed(&graph.operation_by_name_required("min_size")?, 0, &min_size);
+    args.add_feed(
+        &graph.operation_by_name_required("thresholds")?,
+        0,
+        &thresholds,
+    );
+    args.add_feed(&graph.operation_by_name_required("factor")?, 0, &factor);
+    args.add_feed(&graph.operation_by_name_required("input")?, 0, &input);
+
+    // Request the following outputs after the session runs.
+    let bbox = args.request_fetch(&graph.operation_by_name_required("box")?, 0);
+    let session = Session::new(&SessionOptions::new(), &graph)?;
+    session.run(&mut args)?;
+
+    // Our bounding box extents.
+    let bbox_res: Tensor<f32> = args.fetch(bbox)?;
+
+    // Print results.
+    let mut iter = 0;
+    let mut json_vec: Vec<[f32; 4]> = Vec::new();
+    while (iter * 4) < bbox_res.len() {
+        json_vec.push([
+            bbox_res[4 * iter + 1], // x1
+            bbox_res[4 * iter],     // y1
+            bbox_res[4 * iter + 3], // x2
+            bbox_res[4 * iter + 2], // y2
+        ]);
+        iter += 1;
+    }
+    let json_obj = json!(json_vec);
+    println!("{}", json_obj.to_string());
+    Ok(())
+}
